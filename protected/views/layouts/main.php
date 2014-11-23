5366be25160ec6d4c0e7f1b848ceb23f5c4acee8
kohone <kohone@kohone-PC>
2014-11-23 21:42:03 +0200

    Добавлен пункт меню "Профиль"

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -18,7 +18,7 @@
         <!-- START PAGE CONTAINER -->
         <div class="page-container page-navigation-top">
 
-            
+
 
             <!-- PAGE CONTENT -->
             <div class="page-content">
@@ -68,104 +68,31 @@
                         </li>
                     <?php endif; ?>
 
-
-
                     <!-- SIGN OUT -->
                     <li class="xn-icon-button pull-right">
                         <a href="#" class="mb-control" data-box="#mb-signout"><span class="fa fa-sign-out"></span></a>                        
                     </li> 
                     <!-- END SIGN OUT -->
-                    <!-- MESSAGES -->
-                    <li class="xn-icon-button pull-right">
-                        <a href="#"><span class="fa fa-comments"></span></a>
-                        <div class="informer informer-danger">4</div>
-                        <div class="panel panel-primary animated zoomIn xn-drop-left xn-panel-dragging ui-draggable">
-                            <div class="panel-heading ui-draggable-handle">
-                                <h3 class="panel-title"><span class="fa fa-comments"></span> Messages</h3>                                
-                                <div class="pull-right">
-                                    <span class="label label-danger">4 new</span>
-                                </div>
-                            </div>
-                            <div class="panel-body list-group list-group-contacts scroll mCustomScrollbar _mCS_2 mCS-autoHide mCS_no_scrollbar" style="height: 200px;"><div id="mCSB_2" class="mCustomScrollBox mCS-light mCSB_vertical mCSB_inside" tabindex="0">
-                                    <div id="mCSB_2_container" class="mCSB_container mCS_y_hidden mCS_no_scrollbar_y" style="position: relative; top: 0px; left: 0px;" dir="ltr">
-                                        <a href="#" class="list-group-item">
-                                            <div class="list-group-status status-online"></div>
-                                            <img src="/assets/images/users/user2.jpg" class="pull-left" alt="John Doe">
-                                            <span class="contacts-title">John Doe</span>
-                                            <p>Praesent placerat tellus id augue condimentum</p>
-                                        </a>
-                                        <a href="#" class="list-group-item">
-                                            <div class="list-group-status status-away"></div>
-                                            <img src="/assets/images/users/user.jpg" class="pull-left" alt="Dmitry Ivaniuk">
-                                            <span class="contacts-title">Dmitry Ivaniuk</span>
-                                            <p>Donec risus sapien, sagittis et magna quis</p>
-                                        </a>
-                                        <a href="#" class="list-group-item">
-                                            <div class="list-group-status status-away"></div>
-                                            <img src="/assets/images/users/user3.jpg" class="pull-left" alt="Nadia Ali">
-                                            <span class="contacts-title">Nadia Ali</span>
-                                            <p>Mauris vel eros ut nunc rhoncus cursus sed</p>
-                                        </a>
-                                        <a href="#" class="list-group-item">
-                                            <div class="list-group-status status-offline"></div>
-                                            <img src="/assets/images/users/user6.jpg" class="pull-left" alt="Darth Vader">
-                                            <span class="contacts-title">Darth Vader</span>
-                                            <p>I want my money back!</p>
-                                        </a>
-                                    </div><div id="mCSB_2_scrollbar_vertical" class="mCSB_scrollTools mCSB_2_scrollbar mCS-light mCSB_scrollTools_vertical" style="display: none;"><div class="mCSB_draggerContainer"><div id="mCSB_2_dragger_vertical" class="mCSB_dragger" style="position: absolute; min-height: 30px; top: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar" style="line-height: 30px;"></div></div><div class="mCSB_draggerRail"></div></div></div></div></div>     
-                            <div class="panel-footer text-center">
-                                <a href="pages-messages.html">Show all messages</a>
-                            </div>                            
-                        </div>                        
-                    </li>
-                    <!-- END MESSAGES -->
-                    <!-- TASKS -->
-                    <li class="xn-icon-button pull-right">
-                        <a href="#"><span class="fa fa-tasks"></span></a>
-                        <div class="informer informer-warning">3</div>
-                        <div class="panel panel-primary animated zoomIn xn-drop-left xn-panel-dragging ui-draggable">
-                            <div class="panel-heading ui-draggable-handle">
-                                <h3 class="panel-title"><span class="fa fa-tasks"></span> Tasks</h3>                                
-                                <div class="pull-right">
-                                    <span class="label label-warning">3 active</span>
-                                </div>
-                            </div>
-                            <div class="panel-body list-group scroll mCustomScrollbar _mCS_3 mCS-autoHide mCS_no_scrollbar" style="height: 200px;"><div id="mCSB_3" class="mCustomScrollBox mCS-light mCSB_vertical mCSB_inside" tabindex="0"><div id="mCSB_3_container" class="mCSB_container mCS_y_hidden mCS_no_scrollbar_y" style="position: relative; top: 0px; left: 0px;" dir="ltr">                                
-                                        <a class="list-group-item" href="#">
-                                            <strong>Phasellus augue arcu, elementum</strong>
-                                            <div class="progress progress-small progress-striped active">
-                                                <div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: 50%;">50%</div>
-                                            </div>
-                                            <small class="text-muted">John Doe, 25 Sep 2014 / 50%</small>
-                                        </a>
-                                        <a class="list-group-item" href="#">
-                                            <strong>Aenean ac cursus</strong>
-                                            <div class="progress progress-small progress-striped active">
-                                                <div class="progress-bar progress-bar-warning" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;">80%</div>
-                                            </div>
-                                            <small class="text-muted">Dmitry Ivaniuk, 24 Sep 2014 / 80%</small>
-                                        </a>
-                                        <a class="list-group-item" href="#">
-                                            <strong>Lorem ipsum dolor</strong>
-                                            <div class="progress progress-small progress-striped active">
-                                                <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100" style="width: 95%;">95%</div>
-                                            </div>
-                                            <small class="text-muted">John Doe, 23 Sep 2014 / 95%</small>
-                                        </a>
-                                        <a class="list-group-item" href="#">
-                                            <strong>Cras suscipit ac quam at tincidunt.</strong>
-                                            <div class="progress progress-small">
-                                                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%;">100%</div>
-                                            </div>
-                                            <small class="text-muted">John Doe, 21 Sep 2014 /</small><small class="text-success"> Done</small>
-                                        </a>                                
-                                    </div><div id="mCSB_3_scrollbar_vertical" class="mCSB_scrollTools mCSB_3_scrollbar mCS-light mCSB_scrollTools_vertical" style="display: none;"><div class="mCSB_draggerContainer"><div id="mCSB_3_dragger_vertical" class="mCSB_dragger" style="position: absolute; min-height: 30px; top: 0px;" oncontextmenu="return false;"><div class="mCSB_dragger_bar" style="line-height: 30px;"></div></div><div class="mCSB_draggerRail"></div></div></div></div></div>     
-                            <div class="panel-footer text-center">
-                                <a href="pages-tasks.html">Show all tasks</a>
-                            </div>                            
-                        </div>                        
-                    </li>
-                    <!-- END TASKS -->
+                    <li class="xn-openable pull-right <?php echo Yii::app()->controller->module->id == 'users' ? 'active' : ''; ?>">
+                        <a href="<?= Y::url('/users'); ?>"><span class="fa fa-user"></span> <span class="xn-text"><?= Yii::app()->user->name; ?></span></a>
+                        <ul>
+                            <li <?php echo Yii::app()->controller->module->id == 'users' && $this->action->id == 'create' ? 'class="active"' : ''; ?>>
+                                <a href="<?= Y::url('/'); ?>"><span class="fa fa-envelope-o"></span> Сообщения</a>                        
+                            </li>
+                            <li <?php echo Yii::app()->controller->module->id == 'users' && $this->action->id == 'index' ? 'class="active"' : ''; ?>>
+                                <a href="<?= Y::url('/'); ?>"><span class="fa fa-pencil"></span> Мои переводы</a> 
+                                <div class="informer informer-warning"><?php echo Docs::countDocs(); ?></div>
+                            </li>
+                            <li <?php echo Yii::app()->controller->module->id == 'users' && $this->action->id == 'index' ? 'class="active"' : ''; ?>>
+                                <a href="<?= Y::url('/'); ?>"><span class="fa fa-star"></span> Закладки</a> 
+                                <div class="informer informer-warning"><?php echo Docs::countDocs(); ?></div>
+                            </li>
+                            <li <?php echo Yii::app()->controller->module->id == 'users' && $this->action->id == 'index' ? 'class="active"' : ''; ?>>
+                                <a href="<?= Y::url('/users/default/view', array('id' => Yii::app()->user->id)); ?>"><span class="fa fa-cogs"></span> Настройки</a> 
+                                <div class="informer informer-warning"><?php echo Docs::countDocs(); ?></div>
+                            </li>
+                        </ul>
+                    </li> 
                 </ul>
                 <!-- END X-NAVIGATION VERTICAL -->                     
 
