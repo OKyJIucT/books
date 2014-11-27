2e2ea7acc15103054ac654b83bc35710c6cabb63
kohone <kohone@kohone-PC>
2014-11-27 23:12:56 +0200

    Фикс верстки (список документов)

--- protected/modules/docs/views/default/_view.php
+++ protected/modules/docs/views/default/_view.php
@@ -2,7 +2,7 @@
     <h3><a href="<?= Y::url('/docs/default/view', array('id' => $data->id)); ?>"><?= $data->title; ?></a></h3>
     <h4><?= $data->title_en; ?></h4>
     <div class="col-md-4">
-        <img src="<?= $data->thumb ? '/thumbs' . Y::getDir($data->date) . $data->thumb : '/thubms/noimage.jpg'; ?>" style="float: left; width: 100%; max-width: 300px;" />
+        <img class="img-responsive mbottom" src="<?= $data->thumb ? '/thumbs' . Y::getDir($data->date) . $data->thumb : '/thubms/noimage.jpg'; ?>" />
     </div>
     <div class="col-md-8 form-horizontal">
         <div class="form-group">
@@ -25,17 +25,12 @@
                 <p class="form-control-static"><a href="<?= Y::url('/users/default/view', array('id' => $data->user->id)); ?>"><?= $data->user->email; ?></a></p>
             </div>
         </div>
-
-        <div class="form-group">
-
-            <div class="col-xs-12">
-                <?= Y::cut($data->text); ?>
-            </div>
-        </div>
-
+    </div>
+    <div class="col-md-12">
+        <?= Y::cut($data->text); ?>
         <div class="clearfix"></div>
 
-        <a href="<?= Y::url('/docs/default/view', array('id' => $data->id)); ?>" class="btn btn-success pull-right">Подробней</a>
+        <a href="<?= Y::url('/docs/default/view', array('id' => $data->id)); ?>" class="btn btn-success pull-right mtop">Подробней</a>
 
     </div>
 </div>
