7e57a7c90f2078782739a7398c55f92425451665
kohone <kohone@kohone-PC>
2014-12-07 19:36:14 +0200

    Yandex Metrika

--- themes/sprflat/views/layouts/main.php
+++ themes/sprflat/views/layouts/main.php
@@ -192,6 +192,6 @@
             );
         }
         ?>
-
+        <!-- Yandex.Metrika counter --><script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript"></script><script type="text/javascript">try { var yaCounter27399752 = new Ya.Metrika({ id:27399752 });} catch(e) { }</script><noscript><div><img src="//mc.yandex.ru/watch/27399752" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
     </body>
 </html>
--- themes/sprflat/views/layouts/no_auth.php
+++ themes/sprflat/views/layouts/no_auth.php
@@ -52,5 +52,6 @@
     </head>
     <body class="login-page pace-done">
         <?php echo $content; ?>
+        <!-- Yandex.Metrika counter --><script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript"></script><script type="text/javascript">try { var yaCounter27399752 = new Ya.Metrika({ id:27399752 });} catch(e) { }</script><noscript><div><img src="//mc.yandex.ru/watch/27399752" style="position:absolute; left:-9999px;" alt="" /></div></noscript><!-- /Yandex.Metrika counter -->
     </body>
 </html>
