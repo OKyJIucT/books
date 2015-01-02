748d8d55ec110f13ffaed46d83b7551460564d04
Kohone <okyjiuct@gmail.com>
2015-01-02 13:57:10 +0200

    console db settings

--- protected/config/console.php
+++ protected/config/console.php
@@ -1,45 +1,30 @@
 <?php
 
-// This is the configuration for yiic console application.
-// Any writable CConsoleApplication properties can be configured here.
-return array(
-    'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
-    'name' => 'My Console Application',
-    // preloading 'log' component
-    'preload' => array('log'),
-    'import' => array(
-        'application.models.*',
-        'application.components.*',
-        'ext.YiiMailer.*',
-    ),
-    // application components
-    'components' => array(
-        'db' => array(
-            'connectionString' => 'mysql:host=localhost;dbname=u0037915_22vk',
-            'enableProfiling' => YII_DEBUG,
-            'username' => 'u0037915_22vk',
-            'password' => '5092503',
-            'charset' => 'utf8',
-//            'schemaCachingDuration' => 86400,
+$db_local = dirname(__FILE__) . '/db-local.php';
+
+return CMap::mergeArray(
+    array(
+        'basePath' => dirname(__FILE__) . DIRECTORY_SEPARATOR . '..',
+        'name' => 'My Console Application',
+        // preloading 'log' component
+        'preload' => array('log'),
+        'import' => array(
+            'application.models.*',
+            'application.components.*',
+            'ext.YiiMailer.*',
         ),
-        // uncomment the following to use a MySQL database
-        /*
-          'db'=>array(
-          'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-          'emulatePrepare' => true,
-          'username' => 'root',
-          'password' => '',
-          'charset' => 'utf8',
-          ),
-         */
-        'log' => array(
-            'class' => 'CLogRouter',
-            'routes' => array(
-                array(
-                    'class' => 'CFileLogRoute',
-                    'levels' => 'error, warning',
+        // application components
+        'components' => array(
+            'log' => array(
+                'class' => 'CLogRouter',
+                'routes' => array(
+                    array(
+                        'class' => 'CFileLogRoute',
+                        'levels' => 'error, warning',
+                    ),
                 ),
             ),
         ),
     ),
+    file_exists($db_local) ? require $db_local : array(),
 );
